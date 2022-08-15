79874894d4f1f18da35365f8f904f76f7862e2ae
supersakana <zacwilliamson@icloud.com>
2022-08-15 10:08:00 -0600

    added basic css template to index page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,17 @@ class CommentsController < ApplicationController
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
-      end
+    end
+
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article), status: 303
+    end
     
-      private
-        def comment_params
+    private
+    def comment_params
           params.require(:comment).permit(:commenter, :body)
-        end
+    end
 end
